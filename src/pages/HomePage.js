1dc659b410f57fb76a770e62ca4794c036698ea1
Robin Collins <info@robincollins.co.uk>
2021-08-02 12:22:58 +0100

    added typography scss page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="HomePage">
+      <header classname="hero">
+        <h1 className="hero-text">
+          Hi, I am 
+          <span>Robin</span>
+        </h1>
+        <p>
+          Add Wording here - That is for resume approximately thirty words maybe an
+          introduction possibly from current CV - highlighting transfereable skills.
+        </p>
+      </header>
     </div>
   )
 }
