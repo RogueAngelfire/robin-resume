fd1418c04b830e8e962ac388fa02228e01adceb0
Robin Collins <info@robincollins.co.uk>
2021-08-03 12:55:22 +0100

    added temp images and added styling

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Title from '../Components/Title';
 import ImageSection from '../Components/ImageSection';
 import SkillsSection from '../Components/SkillsSection';
+import ServicesSection from '../Components/ServicesSection';
+import design from '../media/design.svg';
+import intelligence from '../media/intelligence.svg';
+import gamedev from '../media/game-dev.svg';
+
 
 function AboutPage() {
   return (
@@ -20,6 +25,20 @@ function AboutPage() {
         <SkillsSection skill={'Web Design'} progress={'70%'} width={'70%'} />
         <SkillsSection skill={'UI/Ux Design'} progress={'80%'} width={'80%'} />
       </div>
+
+      <Title title={'Services'} span={'Services'} />
+      <div className="servives-container">
+        <ServicesSection image={design} title={'Web design'} 
+        text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        />
+        <ServicesSection image={intelligence} title={'Artificial Intelligence'} 
+        text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        />
+        <ServicesSection image={gamedev} title={'Game Development'} 
+        text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        />
+    </div>
+
     </div>
   )
 }
