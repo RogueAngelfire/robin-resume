d1b5958d90ff4b3fdc2b14a1d4517d3ce4a65a14
Robin Collins <info@robincollins.co.uk>
2021-08-04 14:40:59 +0100

    Adjusted styles to for better user experience

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import PortflioPage from './Pages/PortfolioPage';
 import BlogsPage from './Pages/BlogsPage';
 import ContactPage from './Pages/ContactPage';
 import {Switch, Route} from 'react-router-dom';
+import { useState } from 'react';
 
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () =>{
+    setNavToggle(!navToggle)
+  }
+
   return (
     <div className="App">
-      <div className={"sidebar"}>
+      <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
         <NavBar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="lines-1"></div>
+        <div className="lines-2"></div>
+        <div className="lines-3"></div>
       </div>
       <div className="main-content">
           <div className="content">
