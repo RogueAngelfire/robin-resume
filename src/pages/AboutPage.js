f8c2680b861c2a4ad0eda83aad161979350c8061
Robin Collins <info@robincollins.co.uk>
2021-08-08 15:36:39 +0100

    Removed skills from about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -12,18 +12,6 @@ function AboutPage() {
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'} />
             <ImageSection />
-            <Title title={'My Skills'} span={'My Skills'} />
-            <div className="skillsContainer">
-                <SkillsSection skill={'JavaScript'} progress={'60%'} width={'60%'} />
-                <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
-                <SkillsSection skill={'React Js'} progress={'70%'} width={'70%'} />
-                <SkillsSection skill={'Django'} progress={'80%'} width={'80%'} />
-                <SkillsSection skill={'Python'} progress={'80%'} width={'80%'} />
-                <SkillsSection skill={'HTML'} progress={'90%'} width={'90%'} />
-                <SkillsSection skill={'CSS'} progress={'90%'} width={'90%'} />
-                <SkillsSection skill={'React-Native'} progress={'40%'} width={'40%'} />
-                <SkillsSection skill={'UI/UX Design'} progress={'90%'} width={'90%'} />
-            </div>
 
             <Title title={'Services'} span={'Services'} />
             <div className="servives-container">
