b25827ea84eb7fd040f3298b263a6050948bb281
Robin Collins <info@robincollins.co.uk>
2021-08-09 10:20:14 +0100

    Added correct text to home resume and about me added small title page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons'; 
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faYoutube} from '@fortawesome/free-brands-svg-icons'; // find alternative
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import {A} from 'react-router-dom';
 
@@ -10,12 +10,16 @@ function HomePage() {
     <div className="HomePage">
       <header className="hero">
         <h1 className="hero-text">
-          Hi, I am 
+          Welcom, I am 
           <span> Robin</span>
         </h1>
         <p className="h-sub-text">
-          Add Wording here - That is for resume approximately thirty words maybe an
-          introduction possibly from current CV - highlighting transfereable skills.
+        Full Stack Developer with particular proficiency in the Front-End. 
+        In early 2021 I qualified with a Diploma in Software Development from Code Institute. 
+        I am currently looking to secure a Junior Front-End Developer position to use and further 
+        develop my ability with HTML, CSS and Bootstrap in a practical and fast-paced environment.
+        My career goal is to assume a role that allows me to further develop my programming skills 
+        with a reputable focused organisation.
         </p>
         <div className="icons">
           <a href="https://www.linkedin.com/in/robin-collins-5a785421/" target="_blank" rel="noreferrer" className="icon-holder">
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Title from '../Components/Title';
 import SkillsSection from "../Components/SkillsSection";
 import ResumeItem from '../Components/ResumeItem';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import SmallTitle from '../Components/SmallTitle';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SchoolIcon from '@material-ui/icons/School';
 
 function ResumePage() {
+    const briefcase = <BusinessCenterIcon />
+    const school = <SchoolIcon />
   return ( 
     <div className="ResumePage">
       <Title title={'Resume'} span={'Resume'} />
-      <Title title={'My Skills'} span={'My Skills'} />
+      <SmallTitle title={'My Skills'} span={'My Skills'} />
       <div className="skillsContainer">
       <SkillsSection skill={'HTML5'} progress={'90%'} width={'90%'} />
                 <SkillsSection skill={'CSS3'} progress={'90%'} width={'90%'} />
@@ -24,49 +28,49 @@ function ResumePage() {
             </div>
             
                 <div className="title">
-                    <Title icon={faFacebook} title={'Work Experience'} />
+                    <SmallTitle icon={briefcase} title={'Work Experience'} />
                 </div>
                 <div className="resume-content">
                     <ResumeItem 
                         year={'2008 - Present'} 
                         title={'Photographer & Designer'}
                         subTitle={'Robin Collins Photography'}
-                        text={`• Liaising with customers taking in requirements and execute the desired service.`}
+                        text={`Liasing with customers taking in requirements and execute the desired service. Undertaken 100's of weddings mainly in the South East of England. Commissioned for the Kent Community Safety Awards in 2013. Provided additional services including Website and Graphic Design. Adobe Creative Cloud Software, Advanced in Photoshop, Lightroom and InDesign. Creativity - Constantly looking for creating new styles and market trends. Attention to Detail - Looking for inconsistancies and breaks in continuity.`}
                     />
                     <ResumeItem 
-                        year={'2015 - Present'} 
-                        title={'Full Stack Developer'}
-                        subTitle={'Microsoft Studios'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
+                        year={'2018 - 2019'} 
+                        title={'Shift Manager'}
+                        subTitle={'Amazon'}
+                        text={`Working in a fast pased enviroment as a shift manager for LCY8 at its launch. Accountable for 80+ staff. Analytical Communication reporting and organising with focus on staff welfare and attendance. Working with Amazon's internal Computer system.`} 
                     />
                     <ResumeItem 
-                        year={'2010 - 2017'} 
-                        title={'User Interface Designer'}
-                        subTitle={'Google Inc'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
+                        year={'2017 - 2019'} 
+                        title={'Driver'}
+                        subTitle={'Skill-Tech Recruitment'}
+                        text={`Driving for Sprint Refrigeration and BCA nationwide. Communicating with Head Office, Clients and Customers to Organise and Arrange Deliveries, Collections and Problem-solving. Long and Anti-social delivery/service times working late into the evening and early hours. Adaptability for consistent changes - Being available at short notice to meet customer needs.`} 
                     />
                 </div>
                 <div className="title">
-                    <Title icon={faFacebook} title={'Educational Qualifications'} />
+                    <SmallTitle icon={school} title={'Education'} />
                 </div>
                 <div className="resume-content ">
                     <ResumeItem 
-                        year={'201 - 2022'} 
-                        title={'Computer Science Degree'}
-                        subTitle={'Sussex University'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
+                        year={'2019 - 2021'} 
+                        title={'Full Stack Software Developer'}
+                        subTitle={'The Code Institute'}
+                        text={`Diploma in Software Development. Accredited by the University of Napier, Edinburgh.`} 
                     />
                     <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'A - Levels'}
-                        subTitle={'Church Hill High'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
+                        year={'1997 - 1999'} 
+                        title={'HND Television & Video Production Technology'}
+                        subTitle={'Greenwich University Campas - West Kent College'}
+                        text={`From Script to Screen producing Documentaries. My final assingment was a Documentary called 'Save our Seas' featuring the enviromental group 'Surfers Against Sewage'`} 
                     />
                     <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'High School Graduation'}
-                        subTitle={'ABC School'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
+                        year={'1994 - 1999'} 
+                        title={'Btec National Diploma in Media Arts Technology'}
+                        subTitle={'West Kent College'}
+                        text={`Film, Televison, Editing, Scripts, Photography, Graphic Design, Computer Graphics, Computer Studies, Light and Sound, Radio and more linked to Media Design.`} 
                     />
                 </div>
             
