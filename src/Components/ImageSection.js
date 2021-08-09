d090fe064700227d0da6b9e10542710d20a2ba2e
Robin Collins <info@robincollins.co.uk>
2021-08-09 11:38:32 +0100

    updated image section

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import about from '../media/about.jpg';
+import about from '../media/about.jpg'; // PDF CV link required at the bottom.
 
 function ImageSection() {
     return (
@@ -8,27 +8,25 @@ function ImageSection() {
                 <img src={about} alt="mannequin"/>
             </div>
             <div className="about-info">
-                <h4>I am<span> a Dude</span></h4>
+                <h4>Fully Qualified<span> Full Stack Developer</span></h4>
                 <p className="about-text">
-                  Another Text area to add detailed information about me. Make sure it is engaging 
-                  and reads well. This only needs to  be on or two paragraphs long. With focus on software development.
+                  I have 13 years of Website Design experience in a variety of methods and programming languages.
+                  In early 2021 I Graduated as a Software Developer with the Code Institute which is accredited by the University of Napier, Edinburgh.
+                  I have linked up with other graduates and currently planning some exciting project for real clients from Animators to cake makers
+                  using the latest technologies in front and back end web development incorporating secure Ecommerce services.
                 </p>
                 <div className="about-details">
                     <div className="left-section">
                         <p>Full Name</p>
                         <p>Age</p>
                         <p>Nationality</p>
-                        <p>Languages</p>
-                        <p>Address</p>
-                        <p>Countries</p>
+                        <p>Location</p>
                     </div>
                     <div className="right-section">
                         <p>: Robin Collins</p>
                         <p>: 46</p>
                         <p>: British</p>
-                        <p>: Computer Ones</p>
-                        <p>: Rochester, Kent</p>
-                        <p>: England</p>
+                        <p>: High Halstow, Rochester, Kent</p>
                     </div>
                 </div>
                 <button className="btn">Download CV</button>
